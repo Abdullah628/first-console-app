9927bd249e5d4a97520c06bc6a79288a4786f9a6
abdullah628 <abdullah628360@gmail.com>
2024-10-30 06:42:43 +0600

    first commit with environment setup

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Test{
+    public static void Main(string[] args){
+        Console.WriteLine("Hello World!");
+        // System.Console.WriteLine("My Name \n Abdullah\t Rafi");
+        Console.WriteLine(23);
+        System.Console.WriteLine("nothing is impossible");
+
+        //variable
+        string name = "Abdul Rafi";
+        int age = 23;
+        Console.WriteLine("My Name is "+name+" and I am "+age+" years old.");
+    }
+}
