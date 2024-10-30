50edbdd4f56f799b1dd0886f87b2223c84c3bbe9
abdullah628 <abdullah628360@gmail.com>
2024-10-30 06:57:19 +0600

    basic c#

--- Program.cs
+++ Program.cs
@@ -2,14 +2,45 @@
 
 class Test{
     public static void Main(string[] args){
-        Console.WriteLine("Hello World!");
+
+        // use of backslash and comment
+        // Console.WriteLine("Hello World!");
         // System.Console.WriteLine("My Name \n Abdullah\t Rafi");
-        Console.WriteLine(23);
-        System.Console.WriteLine("nothing is impossible");
+        // Console.WriteLine(23);
+        // System.Console.WriteLine("nothing is impossible");
+
 
         //variable
+        // int -> 112, -112
+        // double -> 21.2
+        // string -> "hello"
+        // char -> 'a'
+        // bool -> true, false
+
         string name = "Abdul Rafi";
         int age = 23;
         Console.WriteLine("My Name is "+name+" and I am "+age+" years old.");
+
+        //-----------------------------
+        //valid variables name
+        // we can use  1. digits, letter, _
+                    // 2. can't start with digit
+                    // 3. can't use keyword
+                    // 4. can't use space
+                    // 5. can't use special character
+
+
+        // variable naming convention
+            // camelCase
+            // descriptive
+            // meaningful
+            // avoid single character
+
+
+        // const variable
+         // - not changeable
+         // - must be initialize with data
+         // - must be initialize with constant value
+
     }
 }
